ed2214f9a9f3e3fe993f88f4eab8b16ea439d4c7
S Namitha <namisasi2005@gmail.com>
2025-01-29 16:17:28 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,49 @@
+class Node {                            // classNode
+	int data;
+	Node next;
+	 
+	 Node(int data) {                   // constructor - 1
+		this.data=data;
+		this.next=null;
+	}
+}
+
+class LinkedList {
+	Node head,tail;
+	public void insert(int data) {         // insert node
+		Node newNode=new Node(data);
+		//newNode.next=head;
+		if(head==null) {
+			head=newNode;
+			tail=newNode;
+		}
+		else {
+			tail.next=newNode;
+			tail=newNode;
+		}
+	}
+	public void display() {
+		Node temp=head;
+		while(temp!=null) {
+			System.out.println(temp.data);
+			temp=temp.next;
+		}
+	}
+	public void insertAtBegining(int data) {            // InsertAtBegining
+		Node newNode=new Node(data);
+		newNode.next=head;
+		head=newNode;
+	}
+}
+
+public class Main {                                 // 3rd
+	public static void main(String[] args) {
+		LinkedList link=new LinkedList();
+		link.insert(100);          
+		link.insert(200);
+		link.insert(300);
+		link.insertAtBegining(500);
+		link.display();
+	}
+}
+
